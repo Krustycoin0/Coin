d9b0d2b05815a686e6315c6ad220dd2d6a0646fa
Krustycoin0 <133280489+Krustycoin0@users.noreply.github.com>
2024-12-21 12:57:03 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,20 +1,23 @@
-document.addEventListener('DOMContentLoaded', function() {
+<script>
+ document.addEventListener('DOMContentLoaded', function() {
     const counters = document.querySelectorAll('.counter');
     const speed = 200; // Velocità di animazione
 
     counters.forEach(counter => {
-        const updateCount = () => {
-            const target = +counter.getAttribute('data-target');
-            const count = +counter.innerText;
+       const updateCount = () => {
+           const target = parseFloat(counter.getAttribute('data-target')); // Converte stringa in float
+           const count = parseFloat(counter.innerText.replace('$', '')); // Converte stringa in float e rimuove $
             const inc = target / speed;
 
-            if (count < target) {
-                counter.innerText = Math.ceil(count + inc);
-                setTimeout(updateCount, 1);
-            } else {
-                counter.innerText = target;
-            }
+          if (count < target) {
+                const newCount = Math.min(target, count + inc)
+                 counter.innerText = `$${newCount.toFixed(6)}` // Per visualizzare 6 decimali
+                 setTimeout(updateCount, 1);
+           } else {
+                counter.innerText = `$${target.toFixed(6)}`;
+           }
         };
-        updateCount();
-    });
-});
+      updateCount();
+     });
+ });
+</script>
